99c04c090bbdada95421c58555f67b1c94ea774c
fbenke <vagrant@precise64.(none)>
2014-06-27 21:20:00 +0000

    implemented /api/v1/payment/<pk> (mpower only so far), data migration for phone numbers

--- payment/urls.py
+++ payment/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
+from payment import views
 
 
 urlpatterns = patterns(
     '',
 
+    # payments
+    url(r'^(?P<pk>[0-9]+)/$', views.RetrievePayment.as_view(), name='payment-detail'),
+
     # mpower
-    url(r'^mpower/$', 'payment.views.opr_charge', name='transaction-charge'),
+    url(r'^mpower/$', 'payment.views.opr_charge', name='mpower-charge'),
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
